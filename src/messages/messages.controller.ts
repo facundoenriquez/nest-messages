fb033c124dbd1f6edb9142672fae571c46635e1f
Facundo Enriquez <fenriquez@inym.org.ar>
2025-06-26 13:54:59 -0300

    adding rest routes to messages controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+    @Get()
+    listMessages() {
+        return 'This action returns all messages';
+    }
+
+    @Post()
+    createMessage() {
+        return 'This action creates a message';
+    }
+
+    @Get(':id')
+    getMessage() {
+        return 'This action returns a message';
+    }
+}
